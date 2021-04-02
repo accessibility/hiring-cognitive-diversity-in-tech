a6305046b414db97ae0e77888f85ebb53327c2a2
Shawn Thompson <plansmash@me.com>
2021-04-02 09:17:25 -0400

    add keywords to package

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,10 @@
 module.exports = function(config) {
-
-	// Adds the robots.txt file to the site for SEO bot controls
-	config.addPassthroughCopy("./robots.txt");
-
 	return {
 		dir: {
 			input: "./src/pages",
 			output: "dist",
 			includes: "../_includes",
-			data: "../data"
+			data: "../_data"
 		},
 		templateFormats: ["html", "md", "njk", "css"],
 		htmlTemplateEngine: "njk",
